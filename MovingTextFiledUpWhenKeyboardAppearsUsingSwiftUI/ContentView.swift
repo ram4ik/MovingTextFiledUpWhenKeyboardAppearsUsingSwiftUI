569fb0e4e374ee18d2f072cc61f90c4589d30aea
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-09 21:43:53 +0200

    init

--- MovingTextFiledUpWhenKeyboardAppearsUsingSwiftUI/ContentView.swift
+++ MovingTextFiledUpWhenKeyboardAppearsUsingSwiftUI/ContentView.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var txt = ""
+    @State private var value: CGFloat = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            List(0..<100) {_ in
+                Text("Hello from SwiftUI")
+            }
+            TextField("Type Something", text: $txt)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal, 15)
+            }.offset(y: -self.value)
+            .animation(.spring())
+            .onAppear() {
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
+                let value = noti.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                let height = value.height
+                
+                self.value = height
+            }
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (noti) in
+                
+                self.value = 0
+            }
+        }
     }
 }
 
